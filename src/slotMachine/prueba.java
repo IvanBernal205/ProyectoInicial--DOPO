89f20ac5c8eddf99c7dfad81b8ebaed90dbde8c4
Ivan Bernal <ivanandressaber@gmail.com>
2026-08-18 14:00:58 -0500

    Primer commit

---
+++ src/slotMachine/prueba.java
@@ -0,0 +1,16 @@
+package slotMachine;
+import shapes.*;
+
+/**
+ * Write a description of class prueba here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class prueba
+{
+    public static void main(){
+        Circle c = new Circle();
+        c.makeVisible();
+    }
+}
